827bb090c37bf20107797017260bcbba64c1e9c0
Charlie <65603222+charlie-moomoo@users.noreply.github.com>
2021-05-09 08:13:34 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,1 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+    window.location.replace("//charlie-moomoo.github.io/CharlieHandwriting" + location.pathname);
+</script>
+</body>
+</html>
 location.pathname
